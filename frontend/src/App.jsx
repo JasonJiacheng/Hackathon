0a1390cb853d97e9f0d7a211fabcb59fd09e0b7f
Ifigeneia Manolou <ipm33@bath.ac.uk>
2026-03-28 20:33:05 +0200

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,24 @@
-import './App.css'
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Generate from "./pages/Generate";
-import Library from "./pages/Upload";
-import Outfits from "./pages/Outfits";
-import Upload from "./pages/Library";
+import { Routes, Route } from "react-router-dom"; 
+import Home from "./pages/Home"; 
+import Generate from "./pages/Generate"; 
+import Library from "./pages/Library"; 
+import Outfits from "./pages/Outfits"; 
+import Upload from "./pages/Upload"; 
 
-function App() {
-    return(
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/GenerateOutfit" element={<Generate />} />
-        <Route path="/UploadedImages" element={<Library />} />
-        <Route path="/Outfits" element={<Outfits />} />
-        <Route path="/Upload" element={<Upload />} />
-      </Routes>
-    );
+ 
 
-}
+function App() { 
+    return( 
+      <Routes> 
+        <Route path="/" element={<Home />} /> 
+        <Route path="/Generate" element={<Generate />} /> 
+        <Route path="/Library" element={<Library />} /> 
+        <Route path="/Outfits" element={<Outfits />} /> 
+        <Route path="/Upload" element={<Upload />} /> 
+      </Routes> 
+    ); 
+} 
 
-export default App;
+ 
+
+export default App; 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,62 +1,51 @@
-import {useState} from 'react';
-import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
-import {ReactTyped} from "react-typed";
-import {Link} from 'react-router'
-
-
-const Home = () => {
-    // for the menu
-    const [nav, setNav] = useState(false);      // when false menu button
-
-
-    // Event handler
-    const handleNav = () => {
-        setNav(!nav);
-    }
-  
-    return (
-        <div className = "relative">
-            <div className = "flex justify-end">
+import {useState} from 'react'; 
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'; 
+import {Link} from 'react-router-dom'; 
+const Home = () => { 
+    // for the menu 
+    const [nav, setNav] = useState(false); // when false menu button 
+     
+    // Event handler 
+    const handleNav = () => { setNav(!nav); } 
+    
+    return ( 
+        <div className = "relative"> 
+            <div className = "flex justify-end"> 
                 <button className = "rounded w-16 h-16 m-5 text-white hover:scale-110 duration-200 flex items-center justify-center " 
-                        onClick = {handleNav}>
-                    {!nav ? <AiOutlineMenu size = {200}/> : <AiOutlineClose size = {200}/>}
-                </button>
-            </div>
+                onClick = {handleNav}> 
+                    {!nav ? <AiOutlineMenu size = {200}/> : <AiOutlineClose size = {200}/>} 
+                </button> 
+            </div> 
+            
+            <div className = "flex flex-col justify-center text-center"> 
+                <h1 className = "text-white text-5xl font-bold"> Application Name</h1> 
+                {/* Live typing content */} 
+                {/* <div className = "flex items-center"> 
+                <ReactTyped className = "text-2xl font-bold py-2" 
+                strings = {["Don't know what to wear or just bored to pick?"]} 
+                typeSpeed = {120} 
+                backSpeed={120} 
+                loop/> </div> */} 
+            </div> {/* Menu */} 
+            <ul className = {nav ? "fixed top-0 left-0 w-[60%] h-full border-r border-gray-600 bg-black transition-in-out duration-300" 
+                                 : "transition-in-out duration-300 fixed -left-full"}> 
+                <li className = "p-4 uppercase text-white text-2xl border-b border-gray-600"> 
+                    <Link to = "/Upload" > Upload </Link> 
+                </li> 
+                
+                <li className = "p-4 uppercase text-white text-2xl border-b border-gray-600"> 
+                    <Link to = "/Library" > Library </Link> 
+                </li> 
+                
+                <li className = "p-4 uppercase text-white text-2xl border-b border-gray-600"> 
+                    <Link to = "/Outfits" > Outfit </Link> 
+                </li> 
+                
+                <li className = "p-4 uppercase text-white text-2xl border-b border-gray-600"> 
+                    <Link to = "/Generate" > Generate </Link> 
+                </li> </ul> </div> 
+    ); 
 
-            <div className = "flex flex-col justify-center text-center">
-                <h1 className = "text-white text-5xl font-bold"> Application Name</h1>
+}; 
 
-                {/* Live typing content */}
-                {/* <div className = "flex items-center">
-                    <ReactTyped className = "text-2xl font-bold py-2"
-                        strings = {["Don't know what to wear or just bored to pick?"]} 
-                        typeSpeed = {120}
-                        backSpeed={120}
-                        loop/>
-                </div> */}
-            </div>
-
-            {/* Menu */}
-            <ul className = {nav ? "fixed top-0 left-0 w-[60%] h-full border-r border-gray-600 bg-black transition-in-out duration-300" : 
-                                   "transition-in-out duration-300 fixed -left-full"}>
-                <li className = "p-4 uppercase text-white text-2xl border-b border-gray-600">
-                    <Link to = "Upload" > Upload </Link> 
-                </li>
-
-                <li className = "p-4 uppercase text-white text-2xl border-b border-gray-600">
-                    <Link to = "Library" > Library </Link> 
-                </li>
-
-                <li className = "p-4 uppercase text-white text-2xl border-b border-gray-600">
-                    <Link to = "Outfits" > Outfits </Link> 
-                </li>
-
-                <li className = "p-4 uppercase text-white text-2xl border-b border-gray-600">
-                    <Link to = "Generate" > Generate </Link> 
-                </li>
-            </ul>
-        </div>
-    );
-}
-
-export default Home
+export default Home;
